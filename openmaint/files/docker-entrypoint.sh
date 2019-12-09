3bb31af3fbe677161b68a59fb66cfaecd3c6951d
Mark Havens <mark.r.havens@gmail.com>
2019-12-09 11:35:16 -0600

    global changes to app identifier & switched to empty dump file

--- openmaint/files/docker-entrypoint.sh
+++ openmaint/files/docker-entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-cat /dev/null > $CATALINA_HOME/conf/cmdbuild/database.conf
-echo "Edit $CATALINA_HOME/conf/cmdbuild/database.conf"
+cat /dev/null > $CATALINA_HOME/conf/openmaint/database.conf
+echo "Edit $CATALINA_HOME/conf/openmaint/database.conf"
 {
     echo "db.url=jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
-    echo "db.username=cmdbuild"
-    echo "db.password=cmdbuild"
+    echo "db.username=openmaint"
+    echo "db.password=openmaint"
     echo "db.admin.username=$POSTGRES_USER"
     echo "db.admin.password=$POSTGRES_PASS"
-} >> $CATALINA_HOME/conf/cmdbuild/database.conf
+} >> $CATALINA_HOME/conf/openmaint/database.conf
 
 # first init DB, second start with fail
 while ! timeout 1 bash -c "echo > /dev/tcp/$POSTGRES_HOST/$POSTGRES_PORT"; do   
@@ -20,7 +20,7 @@ done
 echo "Init DB"
 { # try
   
-    $CATALINA_HOME/webapps/cmdbuild/cmdbuild.sh dbconfig create $CMDBUILD_DUMP -configfile $CATALINA_HOME/conf/cmdbuild/database.conf
+    $CATALINA_HOME/webapps/openmaint/openmaint.sh dbconfig create $OPENMAINT_DUMP -configfile $CATALINA_HOME/conf/openmaint/database.conf
    
 } || { 
     echo "DB was initiliazed. Use dbconfig recreate or dbconfig drop"
